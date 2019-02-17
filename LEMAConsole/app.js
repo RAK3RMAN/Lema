50dc5d0dd2ab98a429448ecfd8be69f1df1c9617
RAK3RMAN <rak3rman@gmail.com>
2019-02-16 23:27:40 -0600

    Updated README and developed app structure

---
+++ LEMAConsole/app.js
@@ -0,0 +1,84 @@
+
+//===================================================//
+//     --- Initialize Packages and Routers ---       //
+//===================================================//
+
+//Declare Packages
+var express = require('express');
+var morgan = require('morgan');
+var createError = require('http-errors');
+var cookieParser = require('cookie-parser');
+var ip = require('ip');
+
+//Setup External Connections
+let port = process.env.PORT || process.argv[2];
+let lemaengine = process.env.LEMAENGINE || process.argv[3];
+
+//Declare App
+const app = express();
+app.set('view engine', 'ejs');
+
+//Routers
+var authenticationRouter = require('./routes/authentication');
+var materialRouter = require('./routes/material');
+
+//Other Processes Setup
+app.use(cookieParser());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/static', express.static(process.cwd() + '/static'));
+
+//End of Initialize Packages and Routers - - - - - - - -
+
+
+//===================================================//
+//        --- Page Specific Routes/Logic ---         //
+//===================================================//
+
+//Authentication
+app.get('/', authenticationRouter.login);
+app.get('/auth/logout', authenticationRouter.logout);
+app.use('/users', usersRouter);
+
+//Dashboard
+app.get('/dashboard', loginRequired, dashboardRouter.dashMain);
+
+//End of Page Specific Routes/Logic - - - - - - - - - -
+
+
+//===================================================//
+//              --- Error Handlers ---               //
+//===================================================//
+
+//404 - Send to Error Handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// Error Handler Logic
+app.use(function(err, req, res, next) {
+  //Determine Message
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  //Render Error Page
+  res.status(err.status || 500);
+  res.render('pages/error.ejs', { title: 'ERROR' });
+});
+
+//End of Error Handler - - - - - - - - - - - - - - - - -
+
+
+//===================================================//
+//               --- Port Listen ---                 //
+//===================================================//
+
+app.listen(port, function () {
+    console.log('Node.js Startup - NodeNetwork-Admin');
+    console.log('Console Accessable at: ' + ip.address() + port);
+    console.log('LEMAEngine Accessed at: ' + lemaengine);
+});
+
+//End of Port Listen - - - - - - - - - - - - - - - - -
+
+module.exports = app;
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# Lema
+# Lema CE
+### Project Under Development!
 A Raspberry Pi discovery and implementation tool written in Node.js
+
+## Basic Structure & Key Features
+Lema aims to provide a cohesive and powerful solution to interacting with Raspberry Pi's GPIO capabilities. The Lema Agent runs on the Raspberry Pi and communicates with the Lema Engine, which provides a GUI webpage (Lema Console) and API pathways for external integration.
+
+### Key Features
+- Automated Capabilites
+	- Install and Setup
+	- Raspberry Pi discovery
+- Beautiful webpage GUI (Lema Console)
+- User Authentication
+- Fast response time with Websockets
+- API Integrations
+	- Setup Raspberry Pi's waiting for integration
+	- Get/Configure Raspberry Pi GPIO setup
+	- Send GPIO commands to Raspberry Pi
+- More features coming soon!
+
+## Install and Setup
+### Lema Admin Install
+Install Lema Admin on a computer or server that will display the GUI webpage and run the Lema Engine. This machine is recommended to remain active and on the network; if disconnected, the Lema Console and Engine will not be able to control external devices.
+1. Become `root` to run the Lema Agent Installer.
+    - Ubuntu 17.04+
+        - `sudo su`
+    - MacOS 10.7+
+        - `su`
+    - Raspbian
+        - `sudo su -`
+2. Download and run the installer.
+```
+bash <(curl -s https://github.com/RAK3RMAN/Lema/lemadmin-install.sh)
+```
+
+### Lema Agent Install
+Install Lema Agent on a Raspberry Pi for the Lema Engine to be able to control the GPIO pins on the board. 
+1. Become `root` to run the Lema Agent Installer.
+```
+sudo su -
+```
+2. Download and run the installer.
+```
+bash <(curl -s https://github.com/RAK3RMAN/Lema/lemagent-install.sh)
+```
