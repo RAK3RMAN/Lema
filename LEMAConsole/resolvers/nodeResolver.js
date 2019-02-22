f438462229a5f9a29e4e25c3092ebed68c0a1a77
RAK3RMAN <rak3rman@gmail.com>
2019-02-21 22:39:47 -0600

    Add Node functionality, with routes and auth

--- LEMAConsole/resolvers/nodeResolver.js
+++ LEMAConsole/resolvers/nodeResolver.js
@@ -7,16 +7,18 @@ var node = require('../models/nodeModel.js');
 //Create Node Device
 exports.create_node = function (req, res) {
     let newNode = new node(req.body);
-    console.log(JSON.stringify(new_node));
-    //new_node.save(function (err, node) {
+    console.log(JSON.stringify(newNode));
+    console.log(req.body);
+    //newNode.save(function (err, node) {
     //    if (err)
     //       res.send(err);
     //    res.json(node);
     //    console.log('Created Node: ' + node);
     //});
+    res.json(node);
 }
 
 //List Node Devices
-exports.list_nodes = function (req, res) {
-    
-}
+//exports.list_nodes = function (req, res) {
+//    
+//}
--- LEMAConsole/static/js/lema-js/nodeFunct.js
+++ LEMAConsole/static/js/lema-js/nodeFunct.js
@@ -10,20 +10,64 @@ function nodeAddSA() {
         showCancelButton: true,
         progressSteps: ['1', '2', '3']
     }).queue([{
-            title: 'Question 1',
-            text: 'Chaining swal2 modals is easy'
+            title: 'Name of Device',
+            text: 'Description'
         },
-        'Question 2',
-        'Question 3'
+        {
+            title: 'Device IP',
+            text: 'Description'
+        },
+        {
+            title: 'Device Type',
+            text: 'Description',
+            input: 'select',
+            inputOptions: {
+                'raspberryPi': 'Raspberry Pi',
+            }
+        }
     ]).then((result) => {
         if (result.value) {
-            Swal.fire({
-                title: 'All done!',
-                html: 'Your answers: <pre><code>' +
-                    JSON.stringify(result.value) +
-                    '</code></pre>',
-                confirmButtonText: 'Lovely!'
-            })
+            $.ajax({
+                type: "POST",
+                url: "/node/create",
+                data: {
+                    node_name: result.value[0],
+                    node_ip: result.value[1],
+                    node_type: result.value[2]
+                },
+                success: function(data) {
+                    Swal.fire({
+                        title: 'Node Created',
+                        html: 'Parameters sent: <pre><code>' +
+                            JSON.stringify(result.value) +
+                            '</code></pre>',
+                        confirmButtonText: 'OK',
+                        type: 'success'
+                    })
+                },
+                error: function(data) {
+                    if (data.status == 200) {
+                        Swal.fire({
+                            title: 'Node Created',
+                            html: 'Parameters sent: <pre><code>' +
+                                JSON.stringify(result.value) +
+                                '</code></pre>',
+                            confirmButtonText: 'OK',
+                            type: 'success'
+                        })
+                    } else {
+                        console.log(data);
+                        Swal.fire({
+                            title: 'Node was not created...',
+                            html: 'Parameters sent: <pre><code>' +
+                                JSON.stringify(result.value) +
+                                '</code></pre>',
+                            confirmButtonText: 'OK',
+                            type: 'error'
+                        })
+                    }
+                }
+            });
         }
     })
 }
