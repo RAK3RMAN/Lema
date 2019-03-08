3c2dbe31dedb3b4335b153700877fb7971f15b8f
RAk3rman <29184717+RAK3RMAN@users.noreply.github.com>
2019-03-08 14:54:21 -0600

    Removed depreciation warnings LEMAgent

--- LEMAgent/app.js
+++ LEMAgent/app.js
@@ -60,7 +60,8 @@ let socket = require('./resolvers/socketResolver.js');
 
 //Other Processes Setup
 app.use(cookieParser());
-app.use(bodyParser());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
