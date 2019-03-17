b672e65d900d5fd616bc13c03da9dd56aceeaebc
RAK3RMAN <rak3rman@gmail.com>
2019-03-16 23:51:35 -0500

    Node Search Functionality

--- LEMAConsole/static/js/lema-js/nodeFunct.js
+++ LEMAConsole/static/js/lema-js/nodeFunct.js
@@ -10,16 +10,37 @@ const Toast = Swal.mixin({
 });
 let hiddenCount = 0;
 
+//Set Table Settings
+let searchValue = "";
+window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    if (value) {
+        searchValue = decodeURI(value);
+    }
+});
+let tableSettings = {
+    "lengthMenu": [
+        [10, 25, 50, -1],
+        [10, 25, 50, "All"]
+    ],
+    "responsive": true,
+    "search": {
+        "search": searchValue,
+    }
+};
+activeTable = $('#activeTable').DataTable(tableSettings);
+pendingTable = $('#pendingTable').DataTable(tableSettings);
+hiddenTable = $('#hiddenTable').DataTable(tableSettings);
+
 //Node List
 function nodeList() {
     //Get list for active Nodes
+    activeTable.clear();
+    pendingTable.clear();
+    hiddenTable.clear();
     $.ajax({
         type: "GET",
         url: "/api/node/details/all",
         success: function (data) {
-            $('#nodelist').empty();
-            $('#nodepenlist').empty();
-            $('#nodehidden').empty();
             let activeCount = 0;
             let pendingCount = 0;
             hiddenCount = 0;
@@ -38,44 +59,46 @@ function nodeList() {
                     } else {
                         name_element = "<i class=\"far fa-question-circle\" style=\"color: dodgerblue;\"></i> " + value.node_name + "  " + nodeArch
                     }
-                    $('#nodelist').append(
-                        "<tr><td>" + name_element + "</td><td>" + value.node_ip + "</td><td>" + value.node_id + "</td>" +
-                        "<td class=\"td-actions text-right\">\n" +
+                    let tools = ("<div class=\"td-actions text-right\">\n" +
                         "<button type=\"button\" rel=\"tooltip\" class=\"btn btn-info\" data-original-title=\"\" onclick=\"nodeEdit('" + value.node_id + "')\" title=\"\">\n" +
                         "<i class=\"fas fa-edit\"></i> Edit\n" +
                         "<button type=\"button\" rel=\"tooltip\" class=\"btn btn-danger ml-2\" data-original-title=\"\" onclick=\"nodeRelease('" + value.node_id + "')\" title=\"\">\n" +
                         "<i class=\"fas fa-times-circle\"></i> Release\n" +
-                        "</td></tr>"
+                        "</div></div>"
                     );
+                    activeTable.row.add([name_element, value.node_ip, value.node_id, tools]);
                     activeCount += 1;
                 }
                 //If node is pending, append to nodePen table
                 else if (value.node_status === "pending") {
                     let name_element = "<i class=\"fas fa-adjust\" style=\"color: orange;\"></i> " + value.node_name + " | " + value.node_ip;
-                    $('#nodepenlist').append(
-                        "<tr><td>" + name_element + "</td><td>" + value.node_id + "</td><td>" + value.created_date + "(UTC)</td>" +
-                        "<td class=\"td-actions text-right\">\n" +
+                    let created_date = value.created_date + "(UTC)";
+                    let tools = ("<div class=\"td-actions text-right\">\n" +
                         "<button type=\"button\" rel=\"tooltip\" class=\"btn btn-info\" data-original-title=\"\" onclick=\"nodepenActivate('" + value.node_id + "')\" title=\"\">\n" +
                         "<i class=\"material-icons\">add</i> Activate\n" +
                         "<button type=\"button\" rel=\"tooltip\" class=\"btn btn-danger ml-2\" data-original-title=\"\" onclick=\"nodepenHide('" + value.node_id + "')\" title=\"\">\n" +
                         "<i class=\"material-icons\">move_to_inbox</i> Hide\n" +
-                        "</td></tr>"
+                        "</div></div>"
                     );
+                    pendingTable.row.add([name_element, value.node_id, created_date, tools]);
                     pendingCount += 1;
                 } else {
                     let name_element = "<i class=\"fas fa-ban\" style=\"color: grey;\"></i> " + value.node_name + " | " + value.node_ip;
-                    $('#nodehidden').append(
-                        "<tr><td>" + name_element + "</td><td>" + value.node_id + "</td><td>" + value.created_date + "(UTC)</td>" +
-                        "<td class=\"td-actions text-right\">\n" +
+                    let created_date = value.created_date + "(UTC)";
+                    let tools = ("<div class=\"td-actions text-right\">\n" +
                         "<button type=\"button\" rel=\"tooltip\" class=\"btn btn-info\" data-original-title=\"\" onclick=\"nodepenActivate('" + value.node_id + "')\" title=\"\">\n" +
                         "<i class=\"material-icons\">add</i> Activate\n" +
-                        "</td></tr>"
+                        "</div></div>"
                     );
+                    hiddenTable.row.add([name_element, value.node_id, created_date, tools]);
                     hiddenCount += 1;
                 }
             });
             document.getElementById("activeCount").innerHTML = activeCount;
             //document.getElementById("pendingCount").innerHTML = pendingCount;
+            activeTable.draw();
+            pendingTable.draw();
+            hiddenTable.draw();
         },
         error: function (data) {
             console.log(data);
@@ -390,33 +413,9 @@ function nodeSetRange() {
 //Show/Hide Hidden Nodes Table
 let tablestat = 0;
 function toggleHidden() {
-    let basecode = "<div class=\"card\">\n" +
-        "   <div class=\"card-header card-header-tabs card-header-primary\">\n" +
-        "       <div class=\"nav-tabs-navigation\">\n" +
-        "           <div class=\"nav-tabs-wrapper\">\n" +
-        "               <span class=\"nav-tabs-title p-0\">\n" +
-        "                   <h4 class=\"card-title\"><strong>Hidden Nodes</strong></h4>\n" +
-        "                   <p class=\"card-category\">Number of Hidden Nodes: <strong><span id=\"hiddenCount\"></span></strong></p>\n" +
-        "               </span>\n" +
-        "           </div>\n" +
-        "       </div>\n" +
-        "    </div>\n" +
-        "    <div class=\"card-body table-responsive\">\n" +
-        "       <table class=\"table table-hover\">\n" +
-        "           <thead class=\"text-muted\">\n" +
-        "               <th>Hostname/IP</th>\n" +
-        "               <th>Node ID</th>\n" +
-        "               <th>Acquisition Date</th>\n" +
-        "               <th>Tools</th>\n" +
-        "           </thead>\n" +
-        "           <tbody id=\"nodehidden\"></tbody>\n" +
-        "       </table>\n" +
-        "    </div>\n" +
-        "</div>";
     if (tablestat === 0) {
-        $('#hiddenTable').append(
-            basecode
-        );
+        //If hidden, appear
+        document.getElementById("hiddenDiv").style = " ";
         $('#hiddenButton').empty();
         $('#hiddenButton').append(
             "<button class=\"btn btn-muted btn-round\" onclick=\"toggleHidden()\"><i class=\"fas fa-eye-slash\"></i> Hide Hidden Nodes\n" +
@@ -426,15 +425,16 @@ function toggleHidden() {
         nodeList();
         tablestat = 1;
     } else {
-        $('#hiddenTable').empty();
+        //If present, hide
+        document.getElementById("hiddenDiv").style = "display:none";
         $('#hiddenButton').empty();
         $('#hiddenButton').append(
             "<button class=\"btn btn-muted btn-round\" onclick=\"toggleHidden()\"><i class=\"far fa-eye\"></i> Show Hidden Nodes\n" +
             "   <div class=\"ripple-container\"></div>\n" +
             "</button>"
         );
-        tablestat = 0;
         nodeList();
+        tablestat = 0;
     }
     document.getElementById("hiddenCount").innerHTML = hiddenCount;
 }
--- LEMAConsole/static/js/lema-js/notify.js
+++ LEMAConsole/static/js/lema-js/notify.js
@@ -2,6 +2,7 @@
 LEMAConsole Front-End JS - Authored by: RAk3rman
 \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\*/
 
+//Send notification to client given params
 function sendNotification(senticon, sentmessage, senttype) {
     $.notify({
         icon: senticon,
@@ -14,4 +15,10 @@ function sendNotification(senticon, sentmessage, senttype) {
             align: 'right'
         }
     })
+}
+
+//Redirect to Node List page with search param
+function nodeSearch() {
+    let searchValue = document.getElementById("searchValue").value
+    location.assign("/node/list?search=" + searchValue);
 }
